1287f48d2d7dda687f5fd23fd0abfe0e76c883c7
sumayyah <sumayyah16.m@gmail.com>
2014-04-03 08:37:49 -0700

    Fixed unused supportv7 error

--- bluetoothFramework/src/main/java/com/example/bluetoothframework/bluetoothframework/MainActivity.java
+++ bluetoothFramework/src/main/java/com/example/bluetoothframework/bluetoothframework/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.bluetoothframework.bluetoothframework;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
